a400a043323c54714f967dde0965db8290b7becd
olser <gritsienko.oleg@mail.ru>
2022-09-03 16:58:15 +0300

    swiper works in progress, arrows is ready

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,9 +1,42 @@
 import '../css/style.scss'
 
+import Swiper, {Navigation} from "swiper";
+import '../../../node_modules/swiper/swiper.scss';
+import '../../../node_modules/swiper/swiper-bundle.css';
+
+
 
 import burger from "./module/burger";
+import swipers from './module/swipers'
+import initSliders from "./module/swipers";
+
+
+
 // import v from "./module/v";
 
 document.addEventListener("DOMContentLoaded", () => {
   burger()
+  // initSliders()
+  const swiperWork = new Swiper('.works__swiper', {
+    modules: [Navigation],
+    // slidesPerView: 3,
+    spaceBetween: 20,
+    loop: false,
+    slidesPerView: 2,
+    grabCursor: true,
+    centeredSlides: true,
+    initialSlide: 2,
+    navigation: {
+      clickable: true,
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  })
+
+
+
+
+
+
 })
